34a6f2430590cc1f190c0d8a48121923aaba0cd7
Anthony Legarte Lobiano <allobiano@vauldex.com>
2017-04-09 09:47:18 +0800

    edit

--- app/models/package.scala
+++ app/models/package.scala
@@ -5,37 +5,40 @@ import models.domain._
 package object models {
   implicit def getByCountry = new Writes [(Country, Region, Province, Municipality)] {
     def writes(getByCountry: (Country, Region, Province, Municipality)) =
-      Json.obj(
-        "countryId" -> getByCountry._1.id,
-        "countryName" -> getByCountry._1.name,
-        "regionId" -> getByCountry._2.id,
-        "regionName" -> getByCountry._2.name,
-        "provinceId" -> getByCountry._3.id,
-        "provinceName" -> getByCountry._3.name,
-        "municipalityId" -> getByCountry._4.id,
-        "municipalityName" -> getByCountry._4.name
-      )
+      getByCountry match {  case (country, region, province, municipality) =>
+        Json.obj(
+          "countryId" -> country.id,
+          "countryName" -> country.name,
+          "regionId" -> region.id,
+          "regionName" -> region.name,
+          "provinceId" -> province.id,
+          "provinceName" -> province.name,
+          "municipalityId" -> municipality.id,
+          "municipalityName" -> municipality.name
+      )}
   }
 
   implicit def getByRegion = new Writes [(Region, Province, Municipality)] {
     def writes(getByRegion: (Region, Province, Municipality)) =
-      Json.obj(
-        "regionId" -> getByRegion._1.id,
-        "regionName" -> getByRegion._1.name,
-        "provinceId" -> getByRegion._2.id,
-        "provinceName" -> getByRegion._2.name,
-        "municipalityId" -> getByRegion._3.id,
-        "municipalityName" -> getByRegion._3.name
-      )
+      getByRegion match { case (region, province, municipality) =>
+        Json.obj(
+          "regionId" -> region.id,
+          "regionName" -> region.name,
+          "provinceId" -> province.id,
+          "provinceName" -> province.name,
+          "municipalityId" -> municipality.id,
+          "municipalityName" -> municipality.name
+      )}
   }
 
   implicit def getByProvince = new Writes [(Province, Municipality)] {
     def writes(getByProvince: (Province, Municipality)) =
-      Json.obj(
-        "provinceId" -> getByProvince._1.id,
-        "provinceName" -> getByProvince._1.name,
-        "municipalityId" -> getByProvince._2.id,
-        "municipalityName" -> getByProvince._2.name
-      )
+      getByProvince match { case  (province, municipality) =>
+        Json.obj(
+          "provinceId" -> province.id,
+          "provinceName" -> province.name,
+          "municipalityId" -> municipality.id,
+          "municipalityName" -> municipality.name
+      )}
   }
 }
